4243ffd3ecabbfed8dd267bdcdbf375b986a0100
perrotta <perrotta@ucar.edu>
2021-02-22 14:24:57 -0700

    Testing math funcitonality

---
+++ mathtest.py
@@ -0,0 +1,24 @@
+import math as m
+
+e=m.e
+print(e)
+
+y_natural=m.log(e)
+y_base10=m.log(e,10)
+y_log10=m.log10(e)
+
+print(y_natural, y_base10, y_log10)
+
+deg=180
+rad=deg*m.pi/180
+rads_fromfunc=m.radians(deg)
+
+print(deg,rad,rads_fromfunc)
+
+cos_deg=m.cos(deg)
+cos_rad=m.cos(m.radians(deg))
+print(cos_deg,cos_rad)
+
+
+print(m.factorial(5))
+
